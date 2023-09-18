f0c401598ed13452844559c9c1b6e34e3002b079
Victoria Afuwape <victoriaafuwape@outlook.com>
2023-09-18 12:37:47 -0700

    Updated 0-printf.c file

--- 0-printf.c
+++ 0-printf.c
@@ -1,64 +1,52 @@
 #include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
 
-int _format_printer(char spec, int str, va_list fspec)
-{
-        char character;
-        const char *string;
-
-        if (spec == 'c')
-        {
-                character = va_arg(fspec, int);
-                putchar(character);
-                str++;
-        }
-	else if (spec == 's')
-        {
-                string = va_arg(fspec, const char *);
-                while (*string != '\0')
-                {
-                        putchar(*string);
-                        str++;
-                        string++;
-                }
-        }
-	else if (spec == '%')
-        {
-                putchar('%');
-                str++;
-        }
-
-        return (str);
-}
+/**
+ * _printf - function produces output according to a format
+ * @format: string to be produced
+ *
+ * Return: total count of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
-        int i = 0, counter = 0;
-        int update;
-        char specifier;
-        va_list fspec;
+	int i, counter = 0;
+	char specifier, character;
+	const char *string;
+	va_list fspec;
 
-        va_start(fspec, format);
+	va_start(fspec, format);
 
-        while (format[i] != '\0')
-        {
-                if (format[i] != '%')
-                {
-                        putchar(format[i]);
-                        counter++;
-                }
-
-                if (format[i] == '%')
-                {
-                        i++;
-                        specifier = format[i];
-                        update =  _format_printer(specifier, counter, fspec);
-                        counter = update;
-                }
-                i++;
-        }
-        printf("Total characters printed: %d.\n", counter);
-        va_end(fspec);
-        return (counter);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			counter++;
+		} else
+		{
+			i++;
+			specifier = format[i];
+			if (specifier == 'c')
+			{
+				character = va_arg(fspec, int);
+				_putchar(character);
+				counter++;
+			} else if (specifier == 's')
+			{
+				string = va_arg(fspec, const char *);
+				for (; *string != '\0'; string++)
+				{
+					_putchar(*string);
+					counter++;
+				}
+			} else if (specifier == '%')
+			{
+				_putchar('%');
+				counter++;
+			}
+		}
+	}
+	va_end(fspec);
+	return (counter);
 }
